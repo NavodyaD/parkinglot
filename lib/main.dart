6837ee88f277678154d778207d6c7a9be03cfcf9
Hirusha Nethmina <iamhirusha@gmail.com>
2025-02-07 05:45:18 +0530

    changed logic

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:parkinglot/firebase_options.dart';
-import 'package:parkinglot/screens/owner/dashboard.dart';
+import 'package:parkinglot/screens/admin/admin_dashboard.dart';
 import 'package:parkinglot/screens/user/dashboard.dart';
+import 'package:parkinglot/screens/user/maps_page_parkings.dart';
 import 'package:parkinglot/screens/user/parking_details_page.dart';
 import 'package:provider/provider.dart';
+import 'package:parkinglot/screens/login_page.dart';
+import 'package:parkinglot/screens/signup_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,27 +22,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ParkingDetailsPage(),
+      //home: AdminDashboard(),
+      home: ParkingMapPage(),
     );
-    /*return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => AuthProvider()),
-        ChangeNotifierProvider(create: (_) => ParkingLotProvider()),
-      ],
-      child: MaterialApp(
-        title: 'Smart Parking',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: AuthWrapper(),
-        routes: {
-          '/user-dashboard': (context) => UserDashboard(),
-          '/owner-dashboard': (context) => OwnerDashboard(),
-          //'/profile': (context) => UserProfile(),
-          '/parking-history': (context) => ParkingHistory(),
-        },
-      ),
-    );*/
   }
 }
